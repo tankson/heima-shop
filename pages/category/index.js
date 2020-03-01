122764f5c6b93c734bb705aa2f05e0175c5f6875
tankson <57566808+tankson@users.noreply.github.com>
2020-03-01 20:01:19 +0800

    引入公共搜索框组件

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,5 @@
+import request from "../../utils/request.js"
+
 // pages/category/index.js
 Page({
 
@@ -5,62 +7,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 记录当前点击了哪个菜单
+    current: 0,
+    // 数据列表
+    list: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 请求分类页的数据
+    request({
+      url: "/categories"
+    }).then(res => {
+      // 把数据列表保存到data
+      const { message } = res.data;
+      this.setData({
+        list: message
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 点击切换左边菜单栏时候触发
+  handleClick(e) {
+    // index参数
+    const { index } = e.currentTarget.dataset;
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 保存当前点击的索引到data
+    this.setData({
+      current: index
+    })
   }
 })
