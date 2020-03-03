69bc44ac562344372bf64331e2eba0092b6dfdd1
tankson <57566808+tankson@users.noreply.github.com>
2020-03-03 21:11:54 +0800

    取消按钮隐藏&显示完成

--- pages/search/index.js
+++ pages/search/index.js
@@ -5,6 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 输入框的值
+    inputValue: "",
 
   },
 
@@ -15,52 +17,13 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 监听输入框的输入事件
+  handleInput(e) {
+    // value是输入框的值
+    const { value } = e.detail;
+    this.setData({
+      inputValue: value
+    })
   }
+  
 })
