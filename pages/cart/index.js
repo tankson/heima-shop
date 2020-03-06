30625c24c0fdff4e5eb9ec822e258475b92bf3ec
tankson <57566808+tankson@users.noreply.github.com>
2020-03-06 20:03:13 +0800

    购物车页的收货地址&按钮完成

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,6 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 收货地址
+    address: {},
+    // 本地的商品列表
+    goods: [],
+    // 总价格
+    allPrice: 0
 
   },
 
@@ -12,55 +18,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 获取本地的收货地址
+    this.setData({
+      // 如果本地没有address就等于一个空对象
+      address: wx.getStorageSync("address") || {}
+    })
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })
